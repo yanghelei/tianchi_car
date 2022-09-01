5fe5e48250cce259468b3c991d6bc8d929b795e2
heyray <helei_yang@zju.edu.cn>
2022-09-02 03:40:20 +0800

    encourage collections.

--- rainbow/config.py
+++ rainbow/config.py
@@ -31,20 +31,20 @@ config = dict(
 
     noisy_std=0.1,
     n_step=5,  # the number of steps to look ahead. Default to 1.
-    target_update_freq=1.4e3,  # v1.2: 2e4  v1.3: 2.8e3     v1.4: 1.4e3
+    target_update_freq=1.5e3,  # v1.2: 2e4  v1.3: 2.8e3     v1.4: 1.4e3
 
     epoch=1e7,
-    step_per_epoch=1.4e4,  # the number of transitions collected per epoch
-    step_per_collect=2.8e3,  # trainer will collect "step_per_collect" transitions and do some policy network update repeatedly in each epoch.
-    update_per_step=0.2,
+    step_per_epoch=7000,  # the number of transitions collected per epoch
+    step_per_collect=700,  # trainer will collect "step_per_collect" transitions and do some policy network update repeatedly in each epoch.
+    update_per_step=0.1,
 
     batch_size=256,  # the batch size of sample data, which is going to feed in the policy network
     # hidden_sizes=[128, 128],
 
     training_num=14,  # 用于训练的环境数目
-    test_num=10,  # the number of episodes for one policy evaluation
+    test_num=20,  # the number of episodes for one policy evaluation
 
-    logdir='/myspace/rainbow_v1.4',
+    logdir='/myspace/rainbow_v2.0',
     render=0.0,
 
     prioritized_replay=True,
@@ -59,12 +59,12 @@ config = dict(
 
     action_low=np.array([-pi / 4.0, -0.88]),
     action_high=np.array([pi / 4.0,  0.88]),
-    action_per_dim=(21, 3),
+    action_per_dim=(11, 3),
 
     network=dict(
         sur_dim=7,
         sur_hidden=128,
-        ego_dim=8,
+        ego_dim=9,
         ego_hidden=64,
         total_hidden=256,
         action_hidden=(256, 128),
