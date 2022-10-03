1def614c2cd69d8d95dea19019e3bd9a366fa81e
heyray <helei_yang@zju.edu.cn>
2022-10-03 13:52:48 +0800

    feat: lateral mask.

--- utils/processors.py
+++ utils/processors.py
@@ -343,10 +343,10 @@ class Processor:
             block_y_range = (min(npc_y), max(npc_y))
 
             if block_x_range[0] < min(car_x) < block_x_range[1] or block_x_range[0] < max(car_x) < block_x_range[1]:  # 如果 car 和 npc 并排前行
-                if max(car_y) < block_y_range[0]:  # car 在 npc 的左侧
+                if min(car_y) < block_y_range[0]:  # car 在 npc 的左侧
                     if safe_distance < 0.82 and curr_yaw > 0:  # 小于安全距离并且车头仍朝右
                         steer_masks[1] = False  # 右转屏蔽
-                elif min(car_y) > block_y_range[1]:  # car 在 npc 的右侧
+                elif max(car_y) > block_y_range[1]:  # car 在 npc 的右侧
                     if safe_distance < 0.82 and curr_yaw < 0:  # 小于安全距离并且车头仍朝左
                         steer_masks[0] = False  # 左转屏蔽
 
@@ -408,7 +408,7 @@ class Processor:
 
         if min(car_y) < 1:  # 车辆压左线
             steer_prime_mask = self.action_library[:, 0] < 0
-        elif max(car_y) > 1 + 3.75 * 3 > 0:  # 车辆压右线
+        elif max(car_y) > 1 + 3.75 * 3:  # 车辆压右线
             steer_prime_mask = self.action_library[:, 0] > 0
         else:
             steer_prime_mask = np.ones((len(self.action_library),), dtype=np.bool_)
